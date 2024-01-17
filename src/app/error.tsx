3db7c7b03a527b31d24727a45f871af645fa0ccc
ohbyul <b.oh@urbanisom.com>
2024-01-17 17:43:09 +0900

    type interface 차이 설명

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -8,6 +8,12 @@ type Props = {
     reset: () => void
 }
 
+
+// type은 모든 타입을 선언할 때 사용할 수 있고,  + 원시값
+// interface는 객체에 대한 타입을 선언할 때만 사용할 수 있다.
+// 확장 불가능한 타입을 선언하고 싶다면 type을 사용하면 되고, 
+// 확장 가능한 타입을 선언하고 싶다면 interface를 사용하면 된다.
+
 // Error 컴포넌트는 반드시 uc 여야 한다.
 export default function Error({ error, reset }: Props) {
 
